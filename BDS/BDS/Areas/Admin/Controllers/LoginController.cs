260870a8f22742aca8d93470ccc63fd01c5cacfe
NAMPPP <87806981+NAMPPP@users.noreply.github.com>
2022-04-04 22:08:11 +0700

    commit

---
+++ BDS/BDS/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,228 @@
+﻿using AFModels;
+using BDS.DAO;
+using BDS.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BDS.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        BatDongSanContext db = new BatDongSanContext();
+        LoginDAO loginDao = new LoginDAO();
+        [Authorize]
+        public ActionResult Index()
+        {
+            if (Session["User"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            string sTaiKhoan = f["txtTaiKhoan"].ToString();
+            string sMatKhau = f.Get("txtMatKhau").ToString();
+            QuanTri qt = (from a in db.QuanTris
+                          where a.UserName == sTaiKhoan && a.Pw == sMatKhau
+                          select a).SingleOrDefault();
+            if (qt != null)
+            {
+                if (qt.MaPQ == 1)
+                {
+                    ViewBag.ThongBao = "Hi, Admin:" +qt.UserName;
+                    FormsAuthentication.SetAuthCookie(qt.UserName, false);
+                    Session["User"] = qt.UserName;
+                    Session["Pw"] = qt.Pw;
+                    return RedirectToAction("Index","Login");
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Hi " + qt.UserName;
+                    FormsAuthentication.SetAuthCookie(qt.UserName, false);
+                    Session["User"] = qt.UserName;
+                    Session["Pw"] = qt.Pw;
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            ViewBag.ThongBao = "Tên tài khoản hoặc mật khẩu không đúng!";
+            return View();
+        }
+
+
+        public ActionResult ChangePass()
+        {
+            return View();
+        }
+        public ActionResult ForgotPass()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult ChangePass(FormCollection f)
+        {
+            string sUser = Session["User"].ToString();
+            string sPw = Session["Pw"].ToString();
+            string sMatKhauCu = f.Get("txtMatKhau").ToString();
+            string reMatKhau = f.Get("txtreMatKhau").ToString();
+            QuanTri qt = (from a in db.QuanTris
+                          where a.UserName == sUser && a.Pw == sMatKhauCu
+                          select a).SingleOrDefault();
+            if (qt != null)
+            {
+                if(sPw==sMatKhauCu)
+                { 
+
+                if (ModelState.IsValid)
+                    {
+                        var dao = new QuanTriDAO();
+                        if (dao.ChangePw(qt, reMatKhau))
+                        {
+                            Session["Pw"] = reMatKhau;
+                            return RedirectToAction("Index", "Login");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Cập nhật không thành công");
+                        }
+                    }
+            }
+                return View("ChangePass");
+            }
+            ViewBag.ThongBao = "Mật khẩu không đúng!";
+            return View();
+        }
+
+
+        public ActionResult Logout()    
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+        int RandomCode()
+        {
+            int Numrd;
+            Random rd = new Random();
+            Numrd = rd.Next(1000, 9999);
+
+            return Numrd;
+        }
+        [HttpPost]
+        public ActionResult ForgotPass(FormCollection f)
+        {
+            if (ModelState.IsValid)
+            {
+                string sTaiKhoan = f["txtTaiKhoan"].ToString();
+                string sGmail = f.Get("txtGmail").ToString();
+                NhanVien nv = (from a in db.NhanViens
+                               where a.UserName == sTaiKhoan && a.Email == sGmail
+                               select a).SingleOrDefault();
+                if (nv != null)
+                {
+                    QuanTri qt = (from a in db.QuanTris
+                                   where a.UserName == sTaiKhoan
+                                   select a).SingleOrDefault();
+
+
+                    String tn = "<div><h1>Kính chào Quý khách:\t"+ sGmail +"</h1><i>Quý khách nhận được email này, vì đã tiến hành thực hiện lấy lại tài khoản trên hệ thống</i></div>" +
+                        "<h2>Thông tin tài khoản:</h2>" + "<p>Tài khoản:\t" + sTaiKhoan + "</p><p>Mật khẩu:\t" + qt.Pw + "</p><p>Thời gian:\t" + DateTime.Now + "</p><h3></h3><p>Cảm ơn Quý khách đã sử dụng dịch vụ của chúng tôi!</p><p>Mọi thắc mắc xin vui lòng liên hệ CTTNHH BDS DN quaemail: hotro@gmail.com hoặc số hotline: 0396 342 804</p><p>Trân trọng!</p>";
+                    
+                    Send(sGmail, "CTTNHH BDS DN", tn);
+                    return RedirectToAction("Login", "Login");
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Bạn đã nhập sai gmail hoặc gmail này chưa đăng ký!";
+                }    
+            }
+            
+            return View();
+        }
+        
+        public ActionResult Code()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Code(FormCollection f)
+        {
+            if (ModelState.IsValid)
+            {
+                string sCode =  f["txtCode"].ToString();
+                
+                if ( int.Parse(sCode) == RandomCode())
+                {
+                    ViewBag.ThongBao = "Bạn nhập mã code đúng!";
+                    //return RedirectToAction("ChangePass", "Login");
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Bạn nhập mã code sai!";
+                }
+            }
+            return View();
+        }
+
+        private static readonly string _from = "nguyenphuongnam989@gmail.com"; // Email của Sender (của bạn)
+        private static readonly string _pass = "npkrqcfsrklkeurw"; // Mật khẩu Email của Sender (của bạn)
+        public string Send(string sendto, string subject, string content)
+        {
+            //sendto: Email receiver (người nhận)
+            //subject: Tiêu đề email
+            //content: Nội dung của email, bạn có thể viết mã HTML
+            //Nếu gửi email thành công, sẽ trả về kết quả: OK, không thành công sẽ trả về thông tin l�-i
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(_from);
+                mail.To.Add(sendto);
+                mail.Subject = subject;
+                mail.IsBodyHtml = true;
+                mail.Body = content;
+
+                mail.Priority = MailPriority.High;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(_from, _pass);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ BDS/BDS/Areas/User/Controllers/UserController.cs
@@ -0,0 +1,361 @@
+﻿using AFModels;
+using BDS.DAO;
+using BDS.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BDS.Areas.User.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User/User
+        BatDongSanDAO batdongsanDAO = new BatDongSanDAO();
+        LienHeDAO lienheDAO = new LienHeDAO();
+        BatDongSanContext db = new BatDongSanContext();
+        //----------------------------List ra danh sách Bất Động Sản---------------------
+        public ActionResult Index()
+        {
+            List<BatDongSanViewModel> kq = (from bds in db.BatDongSans
+                                            join ct in db.ChiTietBatDongSans
+                                             on bds.MaBDS equals ct.MaBDS
+                                            join n in db.NhomLoais
+                                            on bds.MaNhom equals n.MaNhom
+                                            where bds.Duyet == 1
+                                            select new BatDongSanViewModel()
+                                            {
+                                                MaBDS = bds.MaBDS,
+                                                TenBDS = bds.TenBDS,
+                                                MaNhom = bds.MaNhom,
+                                                MaLoaiBDS = bds.MaLoaiBDS,
+                                                NgayDang = bds.NgayDang,
+                                                UserName = bds.UserName,
+                                                Duyet = bds.Duyet,
+                                                Gia = ct.Gia,
+                                                Mota = ct.Mota,
+                                                Anh = ct.Anh,
+                                                PhongNgu = ct.PhongNgu,
+                                                PhongTam = ct.PhongTam,
+                                                TenNhom = n.TenNhom
+                                            }).ToList();
+            return View(kq);
+        }
+
+        //------------------------------Xem chi tiết------------------------------------
+
+        public ActionResult Detail(int? id)
+        {
+            if (id.HasValue)
+            {
+                ChiTietBDSViewModel p = batdongsanDAO.Detail(id.Value);
+                return View(p);
+            }
+            else
+                return new HttpNotFoundResult("Không tìm thấy trang này!");
+        }
+
+
+
+
+        public ViewResult XemChiTiet(int MaBDS = 0)
+        {
+            BatDongSan hoa = db.BatDongSans.SingleOrDefault(n => n.MaBDS == MaBDS);
+            if (hoa == null)
+            {
+                //Trả về trang báo lỗi 
+                Response.StatusCode = 404;
+                return null;
+            }
+            ViewBag.TenDM = db.ChiTietBatDongSans.Single(n => n.MaBDS == hoa.MaBDS).Gia;
+            return View(hoa);
+        }
+
+
+
+        NhanVienDAO nhanvienDAO = new NhanVienDAO();
+        public ActionResult NhanVien(int pageNumber = 1, int pageSize = 5)
+        {
+            var pt = nhanvienDAO.ListAll(pageNumber, pageSize);
+            return View(pt);
+        }
+
+        BinhLuanDAO binhLuanDAO = new BinhLuanDAO();
+        public ActionResult BinhLuan(int pageNumber = 1, int pageSize = 5)
+        {
+            var pt = binhLuanDAO.ListAll(pageNumber, pageSize);
+            return View(pt);
+        }
+        //-------------------------------------------LOGIN-------------------------------------------
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            string sTaiKhoan = f["txtTaiKhoan"].ToString();
+            string sMatKhau = f.Get("txtMatKhau").ToString();
+            ThanhVien tv = (from a in db.ThanhViens
+                          where a.UserName == sTaiKhoan && a.Pw == sMatKhau
+                          select a).SingleOrDefault();
+            if (tv != null)
+            {
+                    FormsAuthentication.SetAuthCookie(tv.UserName, false);
+                    Session["TV"] = tv;
+                    Session["UserName"] = tv.UserName;
+                    Session["HoTen"] = tv.HoTenTV;
+                    Session["Email"] = tv.Email;
+                    Session["Pw"] = tv.Pw;
+                    Session["MaKH"] = tv.MaTV;
+                     return RedirectToAction("Index", "User");
+            }
+            ViewBag.ThongBao = "Tên tài khoản hoặc mật khẩu không đúng!";
+            return View();
+        }
+        //-------------------------------------------Đăng ký-------------------------------------------
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ThanhVien model)
+        {
+            if (ModelState.IsValid)
+            {
+                string toAddress = model.Email;
+                string subject = "Bất Động Sản CTTNHH BDS DN | Đăng ký";
+                string path = Path.Combine(HttpRuntime.AppDomainAppPath, "C:/Users/Nam/Desktop/BDS/BDS/BDS/MailTemplate/MailRegister.html");
+                string body = System.IO.File.ReadAllText(path);
+                body = body.Replace("{Name}", model.HoTenTV); //replacing the required things  
+                body = body.Replace("{Username}", model.UserName);
+                body = body.Replace("{Password}", model.Pw);
+                //body = "Chúc mừng "+model.HoTenTV+" đã Đăng ký tài khoản thành công ! TenTK : "+model.UserName+"| MK: "+model.Pw;
+
+                var id = new ThanhVienDAO().Insert(model);
+                Send(toAddress, subject, body);
+                if (id > 0)
+                {
+                   
+                    return RedirectToAction("Thongbao","User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm mới không thành công");
+                    
+
+                }
+            }
+            return View(model);
+        }
+
+        //-------------------------------------------Liên hệ với công ty-------------------------------------------
+        [HttpGet]
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LienHe(LienHe model)
+        {
+
+
+            string toAddress = model.Email;
+            string subject = "CTTNHH BDS DN | Liên hệ";
+            string body = "chúng tôi đã nhận được thư của bạn. chúng tôi sẽ phản hồi trong thời gian sớm nhất đến bạn ! lời nhắn từ bạn: " + model.LoiNhan;          
+            string path = Path.Combine(HttpRuntime.AppDomainAppPath, "C:/Users/Nam/Desktop/BDS/BDS/BDS/MailTemplate/MailLienHe.html");
+            body = System.IO.File.ReadAllText(path);
+            body = body.Replace("{Name}", model.HoTen); //replacing the required things  
+            body = body.Replace("{Content}", model.LoiNhan);
+            if (ModelState.IsValid)
+            {
+                var id = new LienHeDAO().Insert(model);
+                Send(toAddress, subject, body);
+                if (id > 0)
+                {
+                   
+                    return RedirectToAction("ThongbaoLienHe","User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Liên hệ không thành công");
+
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+
+        //-------------------------------------- Thêm thông tin khách hàng trước khi liên hệ ---------------------------------
+        [HttpGet]
+        public ActionResult InformationCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InformationCustomer(KhachHang model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var id = new KhachHangDAO().Insert(model);
+                if (id > 0)
+                {
+                    Session["KH"] = model;
+                    Session["MaKH"] = model.MaKH;
+                    return RedirectToAction("DatThue","DatThue");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm mới không thành công");
+                }
+            }
+            return View(model);
+        }
+
+        //-------------------------------------Menu chỉnh sửa các thông tin cá nhân của khách hàng---------------------------
+        [HttpGet]
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+        //--------------------------------------Chỉnh sửa thông tin cá nhân---------------------------------------------------
+
+        [HttpGet]
+        public ActionResult EditInfo()
+        {
+            string user = Session["UserName"].ToString();
+            ThanhVien kq = (from tv in db.ThanhViens
+                            where tv.UserName == user
+                            select tv).SingleOrDefault();
+            return View(kq);
+        }
+
+        [HttpPost]
+        public ActionResult EditInfo(ThanhVien idnv)
+        {
+            if (ModelState.IsValid)
+            {
+                    var tv = db.ThanhViens.Find(idnv.MaTV);
+                    tv.HoTenTV = idnv.HoTenTV;
+                    tv.Pw = idnv.Pw;
+                    tv.DienThoai = idnv.DienThoai;
+                    tv.GioiTinh = idnv.GioiTinh;
+                    tv.DiaChi = idnv.DiaChi;
+                    tv.Email = idnv.Email;
+                    db.SaveChanges();
+                string user = Session["UserName"].ToString();
+                QuanTri kq = (from qt in db.QuanTris
+                                where qt.UserName == user
+                                select qt).SingleOrDefault();
+                kq.Pw = idnv.Pw;
+                db.SaveChanges();
+                return RedirectToAction("Account", "User");
+
+              }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật không thành công");
+                }
+            return View();
+        }
+
+        public ActionResult Thongbao()
+        {
+            return View();
+        }
+
+        public ActionResult ThongbaoLienHe()
+        {
+            return View();
+        }
+        public ActionResult ForgotPass()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ForgotPass(FormCollection f)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                string sTaiKhoan = f["txtTaiKhoan"].ToString();
+                string sGmail = f.Get("txtGmail").ToString();
+                ThanhVien tv = (from a in db.ThanhViens
+                               where a.UserName == sTaiKhoan && a.Email == sGmail
+                               select a).SingleOrDefault();
+                if (tv != null)
+                {
+                    string toAddress = sGmail;
+                    string subject = "Bất Động Sản CTTNHH BDS DN | Đăng ký";
+                    string path = Path.Combine(HttpRuntime.AppDomainAppPath, "C:/Users/Nam/Desktop/BDS/BDS/BDS/MailTemplate/MailForgot.html");
+                    string body = System.IO.File.ReadAllText(path);
+                    body = body.Replace("{Name}", tv.HoTenTV); //replacing the required things  
+                    body = body.Replace("{Username}", tv.UserName);
+                    body = body.Replace("{Password}", tv.Pw);
+                    //body = "Chúc mừng "+model.HoTenTV+" đã Đăng ký tài khoản thành công ! TenTK : "+model.UserName+"| MK: "+model.Pw;
+
+                    Send(toAddress, subject, body);
+                    return RedirectToAction("Login", "User");
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Bạn đã nhập sai gmail hoặc gmail này chưa đăng ký!";
+                }
+            }
+
+            return View();
+        }
+        private static readonly string _from = "nguyenphuongnam989@gmail.com"; // Email của Sender (của bạn)
+        private static readonly string _pass = "npkrqcfsrklkeurw"; // Mật khẩu Email của Sender (của bạn)
+        public string Send(string sendto, string subject, string content)
+        {
+            //sendto: Email receiver (người nhận)
+            //subject: Tiêu đề email
+            //content: Nội dung của email, bạn có thể viết mã HTML
+            //Nếu gửi email thành công, sẽ trả về kết quả: OK, không thành công sẽ trả về thông tin l�-i
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(_from);
+                mail.To.Add(sendto);
+                mail.Subject = subject;
+                mail.IsBodyHtml = true;
+                mail.Body = content;
+
+                mail.Priority = MailPriority.High;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(_from, _pass);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+    }
+}
